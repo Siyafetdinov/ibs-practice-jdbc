7fc68591eace519af2596382c628b3e39496ae79
Siyafetdinov <siyafetdinov@gmail.com>
2023-08-31 01:25:34 +0400

    create test

---
+++ src/test/java/ru/ibs/jdbcTemplate/base/BaseTestJdbc.java
@@ -0,0 +1,11 @@
+package ru.ibs.jdbcTemplate.base;
+import org.junit.jupiter.api.BeforeAll;
+
+public class BaseTestJdbc {
+    protected static DataBaseControlJdbc dataBaseControl = new DataBaseControlJdbc();
+
+    @BeforeAll
+    static void beforeAll() {
+        dataBaseControl.openConnection();
+    }
+}
